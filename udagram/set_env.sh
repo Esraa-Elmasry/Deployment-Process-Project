47bac9df3a487553cb798e76a9e04d6a4e5a075a
Esraa-Elmasry <esraa.elmasry94@gmail.com>
2023-02-05 05:47:27 +0200

    add folders

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=InspectoR9862
+export POSTGRES_HOST=database-1.cosgwpwogbbj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::elasticbeanstalk-us-east-1-537848472277
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=3G7SFk/Xg/229v9DHRjFvCLHNjW63IS9L0KjriTD
+export URL=http://localhost:8080
---
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -0,0 +1,12 @@
+aws s3 cp --recursive ./www s3://ah1-udagram-frontend/
+ POSTGRES_USERNAME=postgres
+ POSTGRES_PASSWORD=InspectoR9862
+ POSTGRES_HOST=database-1.cosgwpwogbbj.us-east-1.rds.amazonaws.com
+ POSTGRES_DB=postgres
+ AWS_BUCKET=arn:aws:s3:::elasticbeanstalk-us-east-1-537848472277
+ AWS_REGION=us-east-1
+ AWS_PROFILE=default
+ JWT_SECRET="hello"
+ URL=http://localhost:8080
+ AWS_ACCESS_KEY_ID=AKIAX2OSEMLKUDG5JTOO
+ AWS_SECRET_ACCESS_KEY=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
